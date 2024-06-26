9da41d3ea17a37caa92b4e0e9a1eb67f114f98da
Alejandrogs19 <alejo4topay@gmail.com>
2024-06-26 18:10:47 -0300

    prints

---
+++ holamundo.py
@@ -0,0 +1,14 @@
+#esto es un comentario
+"""
+este es un tipo de comentario por 
+lineas
+"""
+
+print("hola mundo")
+print("esto es python")
+
+print(type(12))
+print(type(12.3))
+print(type("texto"))
+print(type(12))
+
