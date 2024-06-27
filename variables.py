20875bf5633aa91cda97350e2bd29329efc29b54
Alejandrogs19 <alejo4topay@gmail.com>
2024-06-27 20:52:24 -0300

    variables

---
+++ variables.py
@@ -0,0 +1,26 @@
+#variables
+
+miprimervar = "me llamo aleo y tengo "
+
+misegundavar = 20
+
+tercervar = "años"
+
+print(miprimervar,misegundavar,tercervar)
+
+quiensoy = (miprimervar,misegundavar,tercervar)
+
+print(len(quiensoy))# len sirve para contar
+
+# variables  en una sola linea (no es muy remendada)
+
+nombre,apellido,edad = "alejo","gonzalez",20
+
+print("me llamo:",nombre,apellido,"mi edad es:", edad,"y me gusta la programacion")
+
+andress: str = "hola"
+andress = 45
+
+print(type(andress))
+
+
